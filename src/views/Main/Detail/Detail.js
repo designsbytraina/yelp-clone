f1d8ef20098bb9206cfe1adc0697eb6dbd77a6b5
Rachel Traina-Grandon <racheltrainagrandon@gmail.com>
2016-11-08 11:42:57 -0800

    Added media queries and finalized routing.

--- src/views/Main/Detail/Detail.js
+++ src/views/Main/Detail/Detail.js
@@ -50,6 +50,17 @@ export class Detail extends React.Component {
     });
   }
 
+  renderPhotos(place) {
+    if (!place.photos || place.photos.length == 0) return;
+    const cfg = {maxWidth: 100, maxHeight: 100}
+    return (<div className={styles.photoStrip}>
+      {place.photos.map(p => {
+        const url = `${p.getUrl(cfg)}.png`
+        return (<img key={url} src={url} />)
+      })}
+    </div>)
+  }
+
   render() {
     if (this.state.loading) {
       return (<div className={styles.wrapper}>
@@ -62,6 +73,9 @@ export class Detail extends React.Component {
       <div className={styles.wrapper}>
         <div className={styles.header}>
           <h2>{place.name}</h2>
+        </div>
+        <div className={styles.details}>
+          {this.renderPhotos(place)}
         </div>
       </div>
     )
--- src/views/Main/routes.js
+++ src/views/Main/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route} from 'react-router';
+import {Route, IndexRoute} from 'react-router';
 import Container from './Container';
 import Map from './Map/Map';
 
@@ -10,6 +10,7 @@ export const makeMainRoutes = () => {
       <Route path="map" component={Map} />
       <Route path="detail/:placeId"
             component={Detail} />
+      <IndexRoute component={Map} />
     </Route>
   )
 }
