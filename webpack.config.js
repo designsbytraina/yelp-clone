c807b19925f0efe3ab336f0482ea3589f2b222b1
Rachel Traina-Grandon <racheltrainagrandon@gmail.com>
2016-11-07 16:46:13 -0800

    Added css modules.

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,44 @@ const path    = require('path'),
       join    = path.join,
       resolve = path.resolve;
 
+const cssModulesNames = `${isDev ? '[path][name]__[local]__' : ''}[hash:base64:5]`;
+
+const matchCssLoaders = /(^|!)(css-loader)($|!)/;
+
+const findLoader = (loaders, match) => {
+  const found = loaders.filter(l => l &&
+      l.loader && l.loader.match(match));
+  return found ? found[0] : null;
+}
+// existing css loader
+const cssloader =
+  findLoader(config.module.loaders, matchCssLoaders);
+
+const newloader = Object.assign({}, cssloader, {
+  test: /\.module\.css$/,
+  include: [src],
+  loader: cssloader.loader
+    .replace(matchCssLoaders,
+    `$1$2?modules&localIdentName=${cssModulesNames}$3`)
+})
+
+config.module.loaders.push({
+  test: /\.css$/,
+  include: [modules],
+  loader: 'style!css'
+})
+
+cssloader.test =
+  new RegExp(`[^module]${cssloader.test.source}`)
+cssloader.loader = newloader.loader
+
 const getConfig = require('hjs-webpack');
 const NODE_ENV = process.env.NODE_ENV;
 const isDev = NODE_ENV === 'development';
+
+// alternatively, we can use process.argv[1]
+// const isDev = (process.argv[1] || '')
+//                .indexOf('hjs-dev-server') !== -1;
 
 // var config = getConfig({
 //   in: join(__dirname, 'src/app.js'),
@@ -14,15 +49,17 @@ const isDev = NODE_ENV === 'development';
 //   clearBeforeBuild: true
 // })
 
-// alternatively, we can use process.argv[1]
-// const isDev = (process.argv[1] || '')
-//                .indexOf('hjs-dev-server') !== -1;
-
 var config = getConfig({
   isDev: isDev,
   in: join(src, 'app.js'),
   out: dest,
   clearBeforeBuild: true
 })
+
+config.postcss = [].concat([
+  require('precss')({}),
+  require('autoprefixer')({}),
+  require('cssnano')({})
+])
 
 module.exports = config;
